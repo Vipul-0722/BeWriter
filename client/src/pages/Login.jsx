e75122cfac1c74d96953138a1b6c852732ad1d96
Vipul <gaikwadvipul196@gmail.com>
2023-01-01 16:51:10 +0530

    Fixed endpoint issue

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 const Login = () => {
+
+  const [inputData,setData]=useState({
+    username:'',
+    email:'',
+    password:''
+  });
+
+
+  const handleChange=(e)=>{
+    console.log(e.target.name);
+    setData({...inputData,[e.target.name]:e.target.value})
+  
+  }
+
+   
+
+
+
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form autoComplete='off'>
-        <input type='text' placeholder='username' autoComplete='off' ></input>
+        <input type='text' name="username" onChange={handleChange} placeholder='username' autoComplete='off' ></input>
          
-        <input type='password' placeholder='password'  autoComplete='off' ></input>
+        <input type='password' name="password" onChange={handleChange} placeholder='password'  autoComplete='off' ></input>
          
         <button>Login</button>
              <p>This is error</p>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 const Register = () => {
+
+const [inputData,setData]=useState({
+    username:'',
+    email:'',
+    password:''
+  });
+
+  const handleChange=(e)=>{
+    setData({...inputData,[e.target.name]:e.target.value})
+    console.log(inputData)
+  }
+ 
+
+
+  const submitHandle=async (e)=>{
+    e.preventDefault();
+
+    try{
+       const res=await axios.post("http://localhost:3001/auth/register",inputData)
+       console.log(res);
+      }
+    catch(err){
+      console.log(err)
+    }   
+   }
+
   return (
     <div className='auth'>
       <h1>Register</h1>
       <form autoComplete='off'>
-        <input type='text' placeholder='username'  autoComplete='off' ></input>
-        <input type='email' placeholder='email'  autoComplete='off' ></input> 
-        <input type='password' placeholder='password'  autoComplete='off' ></input>
+        <input type='text' placeholder='username' name="username" onChange={handleChange} autoComplete='off' ></input>
+        <input type='email' placeholder='email' name="email" onChange={handleChange} autoComplete='off' ></input> 
+        <input type='password' placeholder='password' name="password" onChange={handleChange} autoComplete='off' ></input>
          
-        <button>Register</button>
-         <p>This is error</p>
+        <button onClick={submitHandle}>Register</button>
+         <p>This is  error</p>
         <span>Don you have account? <Link to="/login">Login</Link></span>
       </form>
     </div>
