d021faafef2252c3d829dfed7454b927b7ee79cb
Vipul <gaikwadvipul196@gmail.com>
2023-01-01 16:41:39 +0530

    Fixed network issue for register page

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,37 +3,32 @@ import bcrypt from "bcryptjs";
 export const register=(req,res)=>{
 
     // CHECK IF USER Already exist or not
-     console.log(req.body.email);
-     console.log(req.body.password)
-     
-    // const q="SELECT * FROM users WHERE email=? or username= ?";
+    const q="SELECT * FROM users WHERE email=? or username= ?";
 
-    // db.query(q,[req.body.email,req,body.name],(err,data)=>{
-    //       console.log("called")
-    //      if(err)  return res.json(err);
-        
-    //      if(data.length) return res.status(409).json("User already exist")
-        
-    //         console.log("called")
-    //      // hashing the password
- 
-    //      const salt=bcrypt.genSaltSync(10);
-    //      const hash=bcrypt.hashSync(req.body.password,salt);
+    db.query(q,[req.body.email,req.body.name],(err,data)=>{
 
-    //      const q="INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-        
-    //      console.log(hash);
-    //      const values=[
-    //         req.body.username,
-    //         req.body.email,
-    //         hash,
-    //     ]
 
-    //      db.query(q,[values],(err,data)=>{
-    //         if(err)  return res.json(err);
-    //         return res.status(200).json("User created successfully !") 
-    //      })
-    //     })
+         if(err)  return res.json(err);
+        
+         if(data.length) return res.status(409).json("User already exist")
+
+         // hashing the password
+         const salt=bcrypt.genSaltSync(10);
+         const hash=bcrypt.hashSync(req.body.password,salt);
+
+         const q="INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+        
+         const values=[
+            req.body.username,
+            req.body.email,
+            hash,
+        ]
+
+         db.query(q,[values],(err,data)=>{
+            if(err)  return res.json(err);
+            return res.status(200).json("User created successfully !") 
+         })
+        })
 
 
 }
--- api/index.js
+++ api/index.js
@@ -2,14 +2,23 @@ import  express, { application }  from "express";
 import postRoutes from "./routes/posts.js"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
-
+import cors from 'cors';
 
 const app=express();
+
+app.use(cors());
+
+ app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 
 app.use("/auth",authRoutes)
 
-app.listen(5000,()=>{
+app.listen(3001,()=>{
     console.log("Server Started")
 })
