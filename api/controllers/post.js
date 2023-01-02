8e430d9ce907900d7002003c55a047758cf4695f
Vipul <gaikwadvipul196@gmail.com>
2023-01-02 21:56:31 +0530

    implemented viewpost part

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,28 @@
+import {db} from "../db.js"
+export const getAllpost=(req,res)=>{
+     res.json('Post response from controller')
+     const cat=req.query.cat 
+               ? "SELECT *FROM posts WHERE cat=?"
+               : "SELECT *FROM posts";
+    db.query(q,[req.query.cat],(err,data)=>{
+       if(err)  return res.send(err);
+
+       return res.status(200).json(data);
+    })
+}
+
+export const getSinglePost=(req,res)=>{
+     res.json('Post response from controller')
+}
+
 export const addpost=(req,res)=>{
+     res.json('Post response from controller')
+}
+
+export const UpdatePost=(req,res)=>{
+     res.json('Post response from controller')
+}
+
+export const DeletePost=(req,res)=>{
      res.json('Post response from controller')
 }
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,12 @@
 import  express  from "express";
+import { addpost, DeletePost, UpdatePost, getAllpost, getSinglePost } from "../controllers/post.js";
 
 const router=express.Router();
+
+router.get("/",getAllpost);
+router.get("/:id",getSinglePost);
+router.post("/",addpost);
+router.delete("/:id",DeletePost);
+router.put("/:id",UpdatePost);
 
 export default router;
